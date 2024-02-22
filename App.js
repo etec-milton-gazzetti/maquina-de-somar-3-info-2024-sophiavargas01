74364326ab5eb4916ca28e45c452c9398929e3d4
sophiavargas01 <141246280+sophiavargas01@users.noreply.github.com>
2024-02-22 12:43:16 +0000

    first commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+       <Text>3°info 2024</Text>
+       <Text>Etec professor Milton Gazzetti</Text>
+       <Text>Desenvolvimento para despositivos móveis 2</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+
+  },
+});
